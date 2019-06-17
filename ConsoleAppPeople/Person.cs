bc40f68d0e5427c96be75ffb8d725e3c97a428df
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-06-17 16:02:37 +0200

    Add project files.

---
+++ ConsoleAppPeople/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppPeople
+{
+    class Person
+    {// class scope - varibles will get a deafult value
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ' ' + LastName;
+            }
+        }
+        public int Age { get; set; }
+
+        //Constructor - Req. First & Last name of person to create.
+        public Person(string firstName, string lastName)
+        {//Local scope - varibles don´t have a deafult value
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello my name is {FullName} and i am {Age} years old.");
+        }
+    }
+}
---
+++ ConsoleAppPeople/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<Person> people = new List<Person>();
+
+            Person me = new Person("Ulf", "Bengtsson", 37);
+            people.Add(me);
+
+            me.Introduce();
+
+            BirthdayParty(me);
+
+            foreach (Person person in people)
+            {
+                person.Introduce();
+            }
+
+            Console.ReadKey(true);
+        }
+
+        private static void BirthdayParty(Person me)
+        {
+            Console.WriteLine($"Yaye it´s my {++me.Age} birthday!!!");
+        }
+
+        static void OnePerson()
+        {
+            //Person me = new Person();
+            //me.FirstName = "Ulf";
+            //me.LastName = "Bengtsson";
+
+            //Person me = new Person("Ulf", "Bengtsson");
+            Person me = new Person("Ulf", "Bengtsson", 37);
+
+            //Console.WriteLine(me.FirstName + ' ' + me.LastName);
+            //Console.WriteLine(me.FullName);
+
+            me.Introduce();
+
+            Console.ReadKey(true);
+        }
+    }
+}
